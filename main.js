c69a1bada530a87ca426e2d361cef528f9e2a9e1
SeussGuide <SeussGuide@gmail.com>
2018-06-08 18:48:06 -0500

    addFiles

---
+++ main.js
@@ -0,0 +1,47 @@
+function home() {
+	console.log("Showing Home Page...");
+	$(".list").hide();
+	$(".bookview").hide();
+	$(".homepage").show();
+}
+
+function list() {
+	console.log("Showing List Page...");
+	$(".homepage").hide();
+	$(".bookview").hide();
+	$(".list").show();
+}
+
+function bookview() {
+	console.log("Showing Book View Page...");
+	$(".homepage").hide();
+	$(".list").hide();
+	$(".bookview").show();
+}
+
+function init() {
+	home();
+	
+	$("#home").click(function() {
+		home();
+	});
+	
+	$("#list").click(function() {
+		list();
+	});
+}
+
+$(document).ready(function() {
+	var items = [];
+	$.getJSON('main.json', function(data){
+		console.log(data);
+		$.each(data, function(key, val) {
+			items.push(val);
+		});
+	});
+	
+	for(var i in items) {
+		$("#booklist").append(items[i]);
+		console.log(items[i]);
+	}
+});
